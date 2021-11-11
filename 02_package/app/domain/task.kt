a73cb131abe002de94f10ea31aa7723ee6dc4209
Yuichiro Luke Smith <contact@yu-smith.com>
2021-11-11 22:00:16 +0900

    :sparkles: 02 packages

---
+++ 02_package/app/domain/task.kt
@@ -0,0 +1,17 @@
+package app.domain
+
+
+enum class TaskStatus {
+    PENDING,
+    DOING,
+    DONE,
+}
+
+
+class Task constructor(title: String) {
+    var status: TaskStatus = TaskStatus.PENDING
+        private set
+    var title: String = title
+        private set
+
+}
---
+++ 02_package/main.kt
@@ -0,0 +1,8 @@
+import app.domain.*
+
+
+fun main() {
+    val task = Task("")
+//    task.status = TaskStatus.DONE
+    println(task.status)
+}
